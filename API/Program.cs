9955d5c4b73639fecdd1290e7430c35eec078012
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-02-28 10:01:56 -0800

    Add Swagger support and update launch settings for API

--- API/Program.cs
+++ API/Program.cs
@@ -1,17 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
+// builder.Services.AddOpenApi();
+// خطوط مربوط به AddOpenApi() و MapOpenApi() را حذف کن مگر اینکه از کتابخانه خاصی استفاده می کنی
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    // app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
