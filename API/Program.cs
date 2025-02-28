00dd3e628d463a2456b0fd9d27018b447b8b0e69
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-02-28 10:32:04 -0800

    Refactor comments in Program.cs for clarity and organization

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,9 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿// The entry point of the application:
 
-// Add services to the container.
+// Create a builder for the web application:
+var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container (Inject services to the builder object):
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -9,20 +11,20 @@ builder.Services.AddSwaggerGen();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 // builder.Services.AddOpenApi();
 // خطوط مربوط به AddOpenApi() و MapOpenApi() را حذف کن مگر اینکه از کتابخانه خاصی استفاده می کنی
+
+// Build the app:
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline (set middlewares):
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
     // app.MapOpenApi();
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
+// Run the app:
 app.Run();
