8521bec0d131b69b40e321be3b7c156c3f651b01
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-03-02 00:30:47 -0800

    Add GetAllStudents method to StudentsController for retrieving student list

--- API/Controllers/StudentsController.cs
+++ API/Controllers/StudentsController.cs
@@ -7,6 +7,13 @@ namespace API.Controllers;
 [ApiController]
 public class StudentsController : ControllerBase
 {
-
+    // GET: URL => http://localhost:5089/api/students
+    [HttpGet]
+    public IActionResult GetAllStudents()
+    {
+        // name of students comes from database:
+        string[] students = ["Amirhossein", "Zahra", "Reyhaneh", "Mohsen"];
+        return Ok(students);
+    }
 }
 
