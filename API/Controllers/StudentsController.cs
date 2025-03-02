78b554ef45fd7a756ba273bb93b2a99492b049aa
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-03-01 23:35:16 -0800

    Add StudentsController with basic routing setup

---
+++ API/Controllers/StudentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+// URL => http://localhost:5089/api/students
+[Route("api/[controller]")]
+[ApiController]
+public class StudentsController : ControllerBase
+{
+
+}
+
