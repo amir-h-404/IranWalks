701f7303f28241fa12d05f3d87b43b8dea4e42d5
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-03-03 02:16:29 -0800

    Add properties to RegionDto for Id, Code, Name, and optional RegionImageUrl

--- API/Models/DTO/RegionDto.cs
+++ API/Models/DTO/RegionDto.cs
@@ -2,6 +2,9 @@
 {
     public class RegionDto
     {
-
+        public Guid Id { get; set; }
+        public required string Code { get; set; }
+        public required string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
     }
 }
