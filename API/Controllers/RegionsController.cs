c2255376c3683b148dc70c2843c25ad4cd36d7ee
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-03-03 00:18:42 -0800

    Refactor RegionsController to use DbContext and prepare for database integration

--- API/Controllers/RegionsController.cs
+++ API/Controllers/RegionsController.cs
@@ -1,4 +1,5 @@
-﻿using API.Models.Domain;
+﻿using API.Data;
+using API.Models.Domain;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -8,28 +9,19 @@ namespace API.Controllers
     [ApiController]
     public class RegionsController : ControllerBase
     {
+        private readonly IranWalksDbContext _dbContext;
+
+        public RegionsController(IranWalksDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         // GET: URL => http://localhost:5089/api/regions
         [HttpGet]
         public IActionResult GetAll()
         {
-            Region[] regions = 
-                [
-                new Region 
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Tehran Region",
-                    Code = "THN",
-                    RegionImageUrl = "https://en.wikipedia.org/wiki/Tehran#/media/File:North_of_Tehran_Skyline_view.jpg"
-                },
-                new Region
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Mashhad Region",
-                    Code = "MHD",
-                    RegionImageUrl = "https://en.wikipedia.org/wiki/Mashhad#/media/File:Aerial_View_of_Koohsangi_street,_Mashhad,_Iran.png"
-                }
-                ];
-            return Ok(regions);
+            // todo: get all regions from the database
+            return Ok();
         }
     }
 }
