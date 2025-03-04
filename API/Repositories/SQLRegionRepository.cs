a07de1367ab075018a7456413f007a19fbef2a56
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-03-04 05:28:38 -0800

    Implement SQLRegionRepository class and add GetAllAsync method

--- API/Repositories/SQLRegionRepository.cs
+++ API/Repositories/SQLRegionRepository.cs
@@ -1,7 +1,20 @@
-﻿namespace API.Repositories
-{
-    public class SQLRegionRepository
-    {
+﻿using API.Data;
+using API.Models.Domain;
 
+namespace API.Repositories
+{
+    public class SQLRegionRepository : IRegionRepository
+    {
+        private readonly IranWalksDbContext _dbContext;
+
+        public SQLRegionRepository(IranWalksDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Region>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
