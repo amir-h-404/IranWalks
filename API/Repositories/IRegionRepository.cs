a63b28c104ff2e298ad41d8adc4b497151a756a3
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-03-04 05:15:54 -0800

    Add GetAllAsync method to IRegionRepository and create SQLRegionRepository class

--- API/Repositories/IRegionRepository.cs
+++ API/Repositories/IRegionRepository.cs
@@ -1,7 +1,9 @@
-﻿namespace API.Repositories
+﻿using API.Models.Domain;
+
+namespace API.Repositories
 {
     public interface IRegionRepository
     {
-
+        Task<List<Region>> GetAllAsync();
     }
 }
