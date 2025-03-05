c84f5667a01f02ea6c8579d86ba2e38621b996f7
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-03-05 00:24:12 -0800

    Add mapping configuration for UserDto to UserDomain in AutoMapperProfiles

--- API/Mappings/AutoMapperProfiles.cs
+++ API/Mappings/AutoMapperProfiles.cs
@@ -4,6 +4,20 @@ namespace API.Mappings
 {
     public class AutoMapperProfiles : Profile
     {
+        public AutoMapperProfiles()
+        {
+            CreateMap<UserDto, UserDomain>();
+        }
+    }
 
+    // Testing:
+    public class UserDto
+    {
+        public string FullName { get; set; }
+    }
+
+    public class UserDomain
+    {
+        public string FullName { get; set; }
     }
 }
