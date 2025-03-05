ea363ae565f01864b18e334682e43689b6bc11ed
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-03-05 00:33:41 -0800

    Remove unused mapping for UserDto to UserDomain and related test classes

--- API/Mappings/AutoMapperProfiles.cs
+++ API/Mappings/AutoMapperProfiles.cs
@@ -6,20 +6,7 @@ namespace API.Mappings
     {
         public AutoMapperProfiles()
         {
-            CreateMap<UserDto, UserDomain>()
-                .ForMember(x => x.Name, opt => opt.MapFrom(x => x.FullName))
-                .ReverseMap();
+
         }
-    }
-
-    // Testing:
-    public class UserDto
-    {
-        public string FullName { get; set; }
-    }
-
-    public class UserDomain
-    {
-        public string Name { get; set; }
     }
 }
