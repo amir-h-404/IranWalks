b83d3c3331ca1f5d14d40f218e37f7687e730a85
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-03-05 10:43:21 -0800

    Seed Difficulties and Regions tables with predefined data -> Add-Migration "Seeding data for Difficulties and Regions" / Update-Database

--- API/Migrations/20250305183534_Seeding data for Difficulties and Regions.Designer.cs
+++ API/Migrations/20250305183534_Seeding data for Difficulties and Regions.Designer.cs
@@ -4,6 +4,7 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Migrations
 {
     [DbContext(typeof(IranWalksDbContext))]
-    partial class IranWalksDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250305183534_Seeding data for Difficulties and Regions")]
+    partial class SeedingdataforDifficultiesandRegions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ API/Migrations/20250305183534_Seeding data for Difficulties and Regions.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingdataforDifficultiesandRegions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Difficulties",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("4c0f0e32-38ee-47d0-88e4-acbd9062e8da"), "Easy" },
+                    { new Guid("5cbb6365-1d33-4365-8e98-0d84bef71cb3"), "Hard" },
+                    { new Guid("bb1e5d6f-76d1-4573-9e12-a201e5d576be"), "Medium" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "Id", "Code", "Name", "RegionImageUrl" },
+                values: new object[,]
+                {
+                    { new Guid("5258c24f-5731-482d-a30a-551d5736987e"), "GRN", "Gorgan", "https://cdn.pixabay.com/photo/2016/11/29/05/45/aerial-1867092_960_720.jpg" },
+                    { new Guid("54193e0d-36b0-4763-81aa-98a09b5a2345"), "THN", "Tehran", "https://cdn.pixabay.com/photo/2016/11/29/05/45/aerial-1867092_960_720.jpg" },
+                    { new Guid("6816a60c-9e51-4218-ae33-41af66a29d2f"), "BJD", "Birjand", null },
+                    { new Guid("a3a3a3a3-3a3a-3a3a-3a3a-3a3a3a3a3a3a"), "RAS", "Rasht", "https://cdn.pixabay.com/photo/2016/11/29/05/45/aerial-1867092_960_720.jpg" },
+                    { new Guid("b3b3b3b3-3b3b-3b3b-3b3b-3b3b3b3b3b3b"), "KRM", "Kerman", null },
+                    { new Guid("f3b3b3b3-1b3b-4b3b-8b3b-3b3b3b3b3b3b"), "SHR", "Shiraz", "https://cdn.pixabay.com/photo/2016/11/29/05/45/aerial-1867092_960_720.jpg" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("4c0f0e32-38ee-47d0-88e4-acbd9062e8da"));
+
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("5cbb6365-1d33-4365-8e98-0d84bef71cb3"));
+
+            migrationBuilder.DeleteData(
+                table: "Difficulties",
+                keyColumn: "Id",
+                keyValue: new Guid("bb1e5d6f-76d1-4573-9e12-a201e5d576be"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("5258c24f-5731-482d-a30a-551d5736987e"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("54193e0d-36b0-4763-81aa-98a09b5a2345"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("6816a60c-9e51-4218-ae33-41af66a29d2f"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("a3a3a3a3-3a3a-3a3a-3a3a-3a3a3a3a3a3a"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("b3b3b3b3-3b3b-3b3b-3b3b-3b3b3b3b3b3b"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("f3b3b3b3-1b3b-4b3b-8b3b-3b3b3b3b3b3b"));
+        }
+    }
+}
