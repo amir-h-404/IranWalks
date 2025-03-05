916ae0d8009174cbc403e4d549972dd83f53453d
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-03-05 10:33:57 -0800

    Seed Regions table with predefined region data in OnModelCreating method

--- API/Data/IranWalksDbContext.cs
+++ API/Data/IranWalksDbContext.cs
@@ -37,6 +37,54 @@ namespace API.Data
                 ];
             // Seed difficulties to the database:
             modelBuilder.Entity<Difficulty>().HasData(difficulties);
+            // Seed data for Regions:
+            List<Region> regions =
+                [
+                new Region
+                {
+                    Id = Guid.Parse("5258c24f-5731-482d-a30a-551d5736987e"),
+                    Code = "GRN",
+                    Name = "Gorgan",
+                    RegionImageUrl = "https://cdn.pixabay.com/photo/2016/11/29/05/45/aerial-1867092_960_720.jpg"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("6816a60c-9e51-4218-ae33-41af66a29d2f"),
+                    Code = "BJD",
+                    Name = "Birjand",
+                    RegionImageUrl = null
+                },
+                new Region
+                {
+                    Id = Guid.Parse("54193e0d-36b0-4763-81aa-98a09b5a2345"),
+                    Code = "THN",
+                    Name = "Tehran",
+                    RegionImageUrl = "https://cdn.pixabay.com/photo/2016/11/29/05/45/aerial-1867092_960_720.jpg"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("f3b3b3b3-1b3b-4b3b-8b3b-3b3b3b3b3b3b"),
+                    Code = "SHR",
+                    Name = "Shiraz",
+                    RegionImageUrl = "https://cdn.pixabay.com/photo/2016/11/29/05/45/aerial-1867092_960_720.jpg"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("b3b3b3b3-3b3b-3b3b-3b3b-3b3b3b3b3b3b"),
+                    Code = "KRM",
+                    Name = "Kerman",
+                    RegionImageUrl = null
+                },
+                new Region
+                {
+                    Id = Guid.Parse("a3a3a3a3-3a3a-3a3a-3a3a-3a3a3a3a3a3a"),
+                    Code = "RAS",
+                    Name = "Rasht",
+                    RegionImageUrl = "https://cdn.pixabay.com/photo/2016/11/29/05/45/aerial-1867092_960_720.jpg"
+                }
+                ];
+            // Seed regions to the database:
+            modelBuilder.Entity<Region>().HasData(regions);
         }
     }
 }
